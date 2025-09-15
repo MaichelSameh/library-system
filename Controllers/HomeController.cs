333f662c2ccb64a11e682613d2d4c43c4ee73b25
rrobert <rrobert@VCNBEG001>
2025-09-15 18:07:35 +0300

    Designing book and addbook

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace library_system.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
