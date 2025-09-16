fa2a3dfd440ea7ddaa2342902c59907ec878effb
Michael Essam <messam@venicecom2.locale>
2025-09-16 09:20:01 +0200

    creating controllers

---
+++ Controllers/PubblishersController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using library_system.Models;
+
+namespace library_system.Controllers
+{
+    public class PubblishersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PubblishersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pubblishers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pubblishers.ToListAsync());
+        }
+
+        // GET: Pubblishers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pubblisher = await _context.Pubblishers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pubblisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(pubblisher);
+        }
+
+        // GET: Pubblishers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pubblishers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Company")] Pubblisher pubblisher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pubblisher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pubblisher);
+        }
+
+        // GET: Pubblishers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pubblisher = await _context.Pubblishers.FindAsync(id);
+            if (pubblisher == null)
+            {
+                return NotFound();
+            }
+            return View(pubblisher);
+        }
+
+        // POST: Pubblishers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Company")] Pubblisher pubblisher)
+        {
+            if (id != pubblisher.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pubblisher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PubblisherExists(pubblisher.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pubblisher);
+        }
+
+        // GET: Pubblishers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pubblisher = await _context.Pubblishers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pubblisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(pubblisher);
+        }
+
+        // POST: Pubblishers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pubblisher = await _context.Pubblishers.FindAsync(id);
+            if (pubblisher != null)
+            {
+                _context.Pubblishers.Remove(pubblisher);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PubblisherExists(int id)
+        {
+            return _context.Pubblishers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/TipologiesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using library_system.Models;
+
+namespace library_system.Controllers
+{
+    public class TipologiesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TipologiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tipologies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tipologys.ToListAsync());
+        }
+
+        // GET: Tipologies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipology = await _context.Tipologys
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tipology == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipology);
+        }
+
+        // GET: Tipologies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tipologies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description")] Tipology tipology)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tipology);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipology);
+        }
+
+        // GET: Tipologies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipology = await _context.Tipologys.FindAsync(id);
+            if (tipology == null)
+            {
+                return NotFound();
+            }
+            return View(tipology);
+        }
+
+        // POST: Tipologies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description")] Tipology tipology)
+        {
+            if (id != tipology.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tipology);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TipologyExists(tipology.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipology);
+        }
+
+        // GET: Tipologies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipology = await _context.Tipologys
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tipology == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipology);
+        }
+
+        // POST: Tipologies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tipology = await _context.Tipologys.FindAsync(id);
+            if (tipology != null)
+            {
+                _context.Tipologys.Remove(tipology);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TipologyExists(int id)
+        {
+            return _context.Tipologys.Any(e => e.Id == id);
+        }
+    }
+}
