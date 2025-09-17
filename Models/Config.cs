705b502644a977b7470cbc7cc833583f60a1cae0
ahmaadmahmouud394-hub <ahmaadmahmouud394@gmail.com>
2025-09-17 12:47:37 +0300

    Config

---
+++ Models/Config.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace library_system.Models
+{
+    public class Config
+    {
+        [Key]
+        [Required]
+        public int ID {  get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string Description { get; set; } 
+
+        [Required]
+        [MaxLength(300)]
+        public string Code {  get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Value {  get; set; }
+
+    }
+}
